4089e92cfc68d977fa6e7d2943dfca41b323610f
sunzizhi <2877002341@qq.com>
2021-02-20 21:07:40 +0800

    21年2月20号笔记

---
+++ Day21-2-20/3.3比较运算符.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//±È½ÏÔËËã·û
+	//==
+	int a = 10;
+	int b = 20;
+
+	cout << (a == b) << endl;
+
+	//!=
+	
+	cout << (a != b) << endl;
+
+	//>
+	cout << (a > b) << endl;
+
+	//<
+
+	cout << (a < b) << endl;
+
+	//>=
+
+	cout << (a >= b) << endl;
+
+	//<=
+
+	cout << (a <= b) << endl;
+	return 0;
+}
