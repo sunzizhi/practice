5d9fc7236e36dca07d6143637645aada8f46a542
sunzizhi <74781332+sunzizhi@users.noreply.github.com>
2021-03-06 23:05:40 +0800

    21年3月6号笔记

---
+++ Day21-3-6/练习案例2：点和圆的关系/circle.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include"point.h"
+class Circle
+{
+public:
+	//设置半径
+	void setR(int r);
+	//获取半径
+	int getR();
+	//设置圆心
+	void setCenter(Point center);
+	//获取圆心
+	Point getCenter();
+private:
+	int m_R;//半径
+
+	//在类中可以让另一个类 作为本类中的成员
+	Point m_Center;//圆心
+}; 
